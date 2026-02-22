47a5e0b2f288d686a6bdeaf0f12cb53fe196e1da
Isaev Nikita <nikitavegas95@gmail.com>
2026-02-22 22:06:24 +0300

    feat: add css build

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import styles from './index.module.scss';
+import './global.scss'
+
+export function App () {
+
+    return (
+        <div className={styles.div}>
+            test
+        </div>
+    )
+}
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import {someFn} from "./test";
-
-someFn(1231)
-
-document.body.innerHTML = `<div>test</div>`
