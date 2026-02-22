c3ce83960817be7a79277fe7a681aec1d48efe48
Isaev Nikita <nikitavegas95@gmail.com>
2026-02-22 22:40:11 +0300

    feat(routing) react-router-dom adn lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import styles from './index.module.scss';
 import './global.scss'
+import {Routes, Route, Link} from "react-router-dom";
+import {AboutPageLazy} from "./pages/AboutPage/AboutPage.lazy";
+import {MainPageLazy} from "./pages/MainPage/MainPage.lazy";
+import {Suspense} from "react";
 
 export function App () {
 
     return (
         <div className={styles.div}>
-            test
+            <Link to='/about'>
+                AboutPage
+            </Link>
+            <Link to='/'>
+                MainPage
+            </Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageLazy />}/>
+                    <Route path={'/'} element={<MainPageLazy />}/>
+                </Routes>
+            </Suspense>
         </div>
     )
 }
